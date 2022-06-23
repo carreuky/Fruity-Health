d19a84801fff3526a88f21128e9a44b5cc382d37
carreuky <carreuky@gmail.com>
2022-06-23 17:48:12 +0300

    declaring variable with my API key to get my API

---
+++ assets/js/fruit.js
@@ -0,0 +1,7 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '351f4d8624msh78bf695a7c0b1c4p101dc6jsnada3e3e49a87',
+    'X-RapidAPI-Host': 'fit-life-food.p.rapidapi.com'
+	}
+};
