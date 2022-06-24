c24931d8e162b6ebc0551c2b5ff018bae1b17bb7
carreuky <carreuky@gmail.com>
2022-06-24 16:11:13 +0300

    hosting my local API remotely

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Fruit Health
 
-Fruit Health is a web application that allows user to know  the  nutrients in a fruit by clicking on the fruit ,done by Kelvin Nderitu.
+Fruit Health is a web application that allows user to know  the  nutrients in a fruit by clicking on the fruit. Application done by   _Kelvin Nderitu._
 
 # Description
 This is an application done using HTML CSS and Javascript. Allows user to select a fruit  by clicking on fruit. After clicking the fruit, application will show the nutrients of that fruit.
@@ -13,25 +13,26 @@ This is an application done using HTML CSS and Javascript. Allows user to select
 4. Click specific fruit to check nutrients
 5. View the results of your request
 # Gadgets 
-* Access to  a computer, mobile phone or any other garget
+* Access application using computer, mobile phone or any other garget.
  * Internet access
 # Installation Process
 * Click to this link and access my repo https://github.com/carreuky/Fruity-Health
 * Clone the repository  to in a folder of you choice.
 * Cd in to the folder .
-* You should see the files using **ls** CLI command  
-* cd into db.json and run   _json-server --watch db.json_
+* Open the index.html file with any browser
 # Support
-You can get assistance from me by reaching me on my email address carreuky@gmail.com. Response time within 20 minutes.
+```
+You can get assistance from me by doing an email to  carreuky@gmail.com. Response time within 20 minutes. 
+```
 
 # Live Link
-Click this link to view the live web application https://carreuky.github.io/Quiz-Board
+Click this link to view the live web application https://github.com/carreuky/Fruity-Health
 # Languages Used
 * HTML -to develop the structure of the page
 * CSS - to style the User Interface
 * JavaScript - make the page dynamic and lively
 # Contribution
-Open to contribution to make my web page more user friendly. Add more fruits on my API.
+Open to contribution to make my web page more user friendly. Add more fruits on my API. Open discussion available with anyone.
 ****
 # LICENSE
 * Application free of charge.
--- assets/js/fruit.js
+++ assets/js/fruit.js
@@ -1,38 +1,56 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-
+// Declaring my global scobe
+let url = 'https://carreuky.github.io/db.json/db.json'
 let container = document.getElementById('contEach')
 let formContainer = document.getElementById('formContainer')
-let url ='http://localhost:3000/fruit'
+
+//function to fetch my fruits
 function fetchFruits() {
-	fetch('http://localhost:3000/fruit')
-		.then(response => response.json())
-		.then(response => response.forEach(fruit => {
+	fetch(url)
+		.then(res => res.json())
+		.then(resp => {
+			resp.fruit
+			let fruitAll = resp.fruit
+//Getting each element of my array which is an object
+			fruitAll.forEach(fruitEach => {
+				console.log(fruitEach.name)
+				// Data with me
+				let h = document.createElement('h2')
+				let containerCard = document.getElementById('myFruitsContainer')
+				let card = document.createElement('div')
+				h.innerText = fruitEach.name.charAt(0).toUpperCase() + fruitEach.name.slice(1)
+				h.style.paddingLeft = '20px'
+				card.appendChild(h)
+				containerCard.appendChild(card)
+// Creating a mouse over to change when mouse is over my fruit name
+				h.addEventListener('mouseover', () => {
+					h.style.color = '#edd927'
+				})
+				h.addEventListener('mouseout', () => {
+					h.style.color = 'white'
+				})
 
-			let h = document.createElement('h2')
-			let containerCard = document.getElementById('myFruitsContainer')
-			let card = document.createElement('div')
-			h.innerText = fruit.name.charAt(0).toUpperCase() + fruit.name.slice(1)
-			h.style.paddingLeft = '20px'
-			card.appendChild(h)
-			containerCard.appendChild(card)
-			h.addEventListener('mouseover', ()=>{
-				h.style.color='#edd927'
+				h.style.paddingLeft = '20px'
+				card.appendChild(h)
+				containerCard.appendChild(card)
+				h.addEventListener('mouseover', () => {
+					h.style.color = '#edd927'
+				})
+				h.addEventListener('mouseout', () => {
+					h.style.color = 'white'
+
+				})
+				h.addEventListener('click', () => {
+					container.innerHTML = ''
+					formContainer.style.display = 'block'
+					getNutrients(fruitEach.name)
+				})
+				console.log(h)
+				// .catch(err => console.error(err));
 			})
-			h.addEventListener('mouseout',()=>{
-				h.style.color='white'
-			})
-
-			h.addEventListener('click' , ()=>{
-				container.innerHTML = ''
-				formContainer.style.display = 'block'
-				getNutrients(fruit.name)
-			
-
-			})
-
-		}))
-		.catch(err => console.error(err));
+		})
 }
+
+//  click fuction to display fruit list
 let clickFruits = document.getElementById('clickFruit')
 clickFruits.addEventListener('click', (e) => {
 	e.preventDefault()
@@ -42,41 +60,43 @@ clickFruits.addEventListener('click', (e) => {
 	containerCard.style.display = 'block'
 	fetchFruits()
 })
-let bh=document.createElement('h1')
-function displayNutrients(dataFruit,frtNm) {
-	bh.innerText=frtNm
-	bh.style.border='2px solid #edd927'
+// creating a dom for my nutrients
+let bh = document.createElement('h1')
+function displayNutrients(dataFruit, frtNm) {
+	bh.innerText = frtNm
+	bh.style.border = '2px solid #edd927'
 	bh.style
 	formContainer.append(bh)
 	let eachNutrients = document.createElement("p");
-	eachNutrients.style.fontSize='20px'
+	eachNutrients.style.fontSize = '20px'
 	formContainer.append(bh)
 	container.append(eachNutrients)
 	eachNutrients.textContent = dataFruit.charAt(0).toUpperCase() + dataFruit.slice(1) + ' g'
 
 }
+///// Geting the nutrients from my data
 function getNutrients(fruitName) {
 	fetch(url)
-	  .then(function (response) {
-		return response.json();
-	  })
-	  .then(function (data) {
-
-		for(let i = 0; i<data.length;i++){
-			let frtNm = data[i].name
-			if(frtNm === fruitName){
-				let element = (data[i].nutritions)
-				Object.entries(element).forEach(([key, value]) => {
-				let nutrient = `${key} : ${value}`
-				// let bh=document.createElement('h2')
-				// bh.innerText=frtNm
-				console.log(bh)
-
-				displayNutrients(nutrient,frtNm)
-				})
+		.then(function (response) {
+			return response.json();
+		})
+		.then((data) => {
+			let dat = (data.fruit)
+			console.log(fruitName)
+			for (let i = 0; i < dat.length; i++) {
+				let frtNm = dat[i].name
+				if (frtNm === fruitName) {
+					let element = (dat[i].nutritions)
+					Object.entries(element).forEach(([key, value]) => {
+						let nutrient = `${key} : ${value}`
+						// let bh=document.createElement('h2')
+						// bh.innerText=frtNm
+						console.log(bh)
+						displayNutrients(nutrient, frtNm)
+					})
+				}
 			}
-		}
 
-	  });
-   }
-})
+		});
+}
+getNutrients()
